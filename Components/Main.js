7bec96b59c04aabbf534cf317d8320de11b5e4b4
NoamRa <noamraby@gmail.com>
2018-07-07 19:26:45 +0300

    refactor - move methods to services

--- Components/Main.js
+++ Components/Main.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { TouchableOpacity, Text, View, Button, AsyncStorage  } from 'react-native';
 import TitleAndTextInput from './lib/TitleAndTextInput'
+import {
+  formatDate,
+  formatTime,
+  prepareDataForSubmit,
+  generateOauth2URL,
+  generateGoogleSheetsAppendURL,
+} from '../services/services'
 
 class Main extends React.Component {
   constructor(props) {
@@ -8,12 +15,18 @@ class Main extends React.Component {
     this.state = {};
   }
 
+  componentDidMount() {
+    this.initForm();
+  }
+
   initForm = () => {
+    this.now = new Date();
     this.setState({ 
-      dateTime: new Date(),
+      date: formatDate(this.now),
+      time: formatTime(this.now),
       payee: "",
-      amount: 0,
-      
+      amount: "",
+      disableSubmit: false,
     });
   };
 
@@ -34,53 +47,12 @@ class Main extends React.Component {
     }
   };
 
-  prepareDataForSubmit = (rawData, columnOrder) => {
-    /*
-    payload should look like
-    { "values": [[
-        value for column A, value for column B ....
-      ]]
-    }
-    */
-    let payload = [];
-    payload = columnOrder.map((columnValue, idx) => {
-      if (idx === 0) {
-        return Date.now();
-      } else if (rawData[columnValue.toLowerCase()]) {
-        return rawData[columnValue.toLowerCase()];
-      } else {
-        return "";
-      }
-    })
-    return JSON.stringify({ "values": [payload] });
-  };
-
-  queryParamObjToQueryString = (qParamsObj) => {
-    return `?${Object.keys(qParamsObj).map(key => key + '=' + qParamsObj[key]).join('&')}`
-  };
-
-  generateOauth2URL = (client_id, client_secret, refresh_token) => {
-    const qParamsObj = {
-      client_id: client_id,
-      client_secret: client_secret,
-      refresh_token: refresh_token,
-      grant_type: "refresh_token",
-    };
-    const queryString = this.queryParamObjToQueryString(qParamsObj);
-    const url = `https://www.googleapis.com/oauth2/v4/token${queryString}`
-    console.log("GoogleSheetsAppendURL:", url);
-    return url;
-  };
-
-  generateGoogleSheetsAppendURL = (spreadsheetId, range, qParamsObj) => {
-    const queryString = qParamsObj ? this.queryParamObjToQueryString(qParamsObj) : "";
-    const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}:append${queryString}`
-    console.log("GoogleSheetsAppendURL:", url);
-    return url;
-  };
+  validate = () => {
+    this.setState({ disableSubmit: true });
+  }
   
   saveDataToGoogleSheets = () => {
-    const oauth2TokenAPIURL = this.generateOauth2URL(this.props.CLIENT_ID, this.props.CLIENT_SECRET, this.props.REFRESH_TOKEN);
+    const oauth2TokenAPIURL = generateOauth2URL(this.props.CLIENT_ID, this.props.CLIENT_SECRET, this.props.REFRESH_TOKEN);
     return fetch(oauth2TokenAPIURL, {
       method: 'POST',
       headers: {
@@ -96,14 +68,14 @@ class Main extends React.Component {
       return data;
     })
     .then((authData) => {
-      const value = this.prepareDataForSubmit(this.state, this.props.columnOrder);
+      const value = prepareDataForSubmit(this.state, this.props.columnOrder);
       console.log(value)
       const qParamsObj = {
         valueInputOption: "USER_ENTERED",
         includeValuesInResponse: true, 
         access_token: authData.access_token,
       };
-      const gSheetAPIURL = this.generateGoogleSheetsAppendURL(this.props.spreadsheetId, `'${this.props.spreadSheetName}'`, qParamsObj);
+      const gSheetAPIURL = generateGoogleSheetsAppendURL(this.props.spreadsheetId, `'${this.props.spreadSheetName}'`, qParamsObj);
       return fetch(gSheetAPIURL, {
         method: 'POST',
         headers: {
@@ -113,34 +85,35 @@ class Main extends React.Component {
         },
         body: value
       });
-    })
+    }) 
     .then((response) => response.json())
     .then((responseJson) => {
       console.log(responseJson)
       return responseJson;
+    })
+    .then((responseJson) => {
+      alert("Sent to Google Sheets Successfuly")
+      this.initForm()
     })
     .catch((error) => {
       console.error(error);
     })
   };
   
-
-  
-
   render() {
     return (
       <View>
         <Text>Expense Manager</Text>
-        {/* <TouchableOpacity
+        <TouchableOpacity
           onPress={this.openAndroidDatePicker}
         >
           <TitleAndTextInput
             title="Date"
             placeholder="Payee Name"
             // onChangeText={this.handlePayeeChange}
-            value={this.state.dateTime}
+            value={`${this.state.date} ${this.state.time}`}
           />
-        </TouchableOpacity> */}
+        </TouchableOpacity>
 
         <TitleAndTextInput
           title="Payee"
@@ -161,12 +134,13 @@ class Main extends React.Component {
         <Button
           onPress={this.saveDataToGoogleSheets}
           title="Submit"
-          color="#841584"
+          disabled={this.state.disableSubmit}
+          color={"green"}
           style={{paddingTop: 50}}
         />
 
         <View style={{paddingTop: 20}}>
-          <Text>Date: {this.state.dateTime}</Text>
+          <Text>Report Time: {`${this.state.date} ${this.state.time}`}</Text>
           <Text>Payee: {this.state.payee}</Text>
           <Text>Amount: {this.state.amount}</Text>
         </View>
---
+++ services/services.js
@@ -0,0 +1,60 @@
+const formatDate = (dtObj) => {
+  return `${dtObj.toISOString().split('T')[0]}`;
+};
+
+const formatTime = (dtObj) => {
+  return `${dtObj.toISOString().split('T')[1].split('.')[0]}`;
+};
+
+const prepareDataForSubmit = (rawData, columnOrder) => {
+  /*
+  payload should look like
+  { "values": [[
+      value for column A, value for column B ....
+    ]]
+  }
+  */
+  let payload = [];
+  payload = columnOrder.map((columnValue, idx) => {
+    if (idx === 0) {
+      return Date.now();
+    } else if (rawData[columnValue.toLowerCase()]) {
+      return rawData[columnValue.toLowerCase()];
+    } else {
+      return "";
+    }
+  })
+  return JSON.stringify({ "values": [payload] });
+};
+
+const queryParamObjToQueryString = (qParamsObj) => {
+  return `?${Object.keys(qParamsObj).map(key => key + '=' + qParamsObj[key]).join('&')}`
+};
+
+const generateOauth2URL = (client_id, client_secret, refresh_token) => {
+  const qParamsObj = {
+    client_id: client_id,
+    client_secret: client_secret,
+    refresh_token: refresh_token,
+    grant_type: "refresh_token",
+  };
+  const queryString = queryParamObjToQueryString(qParamsObj);
+  const url = `https://www.googleapis.com/oauth2/v4/token${queryString}`
+  console.log("GoogleSheetsAppendURL:", url);
+  return url;
+};
+
+const generateGoogleSheetsAppendURL = (spreadsheetId, range, qParamsObj) => {
+  const queryString = qParamsObj ? queryParamObjToQueryString(qParamsObj) : "";
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}:append${queryString}`
+  console.log("GoogleSheetsAppendURL:", url);
+  return url;
+};
+
+export {
+  formatDate,
+  formatTime,
+  prepareDataForSubmit,
+  generateOauth2URL,
+  generateGoogleSheetsAppendURL,
+}
