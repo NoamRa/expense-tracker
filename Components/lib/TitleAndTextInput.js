6b6f06fc5f2e182412cf5a679beb76c1d81b7dcd
NoamRa <noamraby@gmail.com>
2018-07-08 21:06:48 +0300

    small things

--- Components/lib/TitleAndTextInput.js
+++ Components/lib/TitleAndTextInput.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { StyleSheet, TextInput, View, Text } from 'react-native';
+import { TextInput, View, Text } from 'react-native';
 
 const TitleAndTextInput = (props) => {
   const style = {
     paddingTop: 5,
   }
+
   const TIStyle={
     height: 40, 
     width: 300,
     borderColor: 'gray', 
     borderWidth: 1,
   }
+  
   return (
     <View style={style}>
       {props.title &&
